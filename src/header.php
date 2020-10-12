0d7d6dfad9e77f8c1268d35fcac295ffb2b2bfe2
PharaohOfParis <mohamed.abdelkoddous@metadot.com>
2020-10-12 14:38:08 +0200

    edited the whole code to make the webpage responsive

--- src/header.php
+++ src/header.php
@@ -1,14 +1,36 @@
-<header class="text-center pt-2 pb-4">
+<header>
+    <div class="container">
+        <nav class="navbar navbar-expand-md navbar-dark">
+            <nav class="navbar ">
+                <a class="navbar-brand pr-1 mr-1" href="home.php">
+                    <img src="./images/bamzooka_logo.png" width="50" height="50" alt="Bamzooka Logo" loading="lazy">
+                </a> BAMZOOKA
+            </nav>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-    <ul class="list-inline">
-        <li class="list-inline-item mx-2"><a href="home.php"><img src="/images/bamzooka_logo.png" alt="Bamzooka logo"
-                    width="50px"></a></li>
-        <li class="list-inline-item ml-2 mr-5 pr-5">BAMZOOKA</li>
-        <li class="list-inline-item ml-5 mr-2 pl-5"><a href="home.php">HOME</a></li>
-        <li class="list-inline-item mx-2"><a href="support.php">SUPPORT</a></li>
-        <li class="list-inline-item mx-2"><a href="pricing.php">PRICING</a></li>
-        <li class="list-inline-item mx-2"><a href="sign_in.php">SIGN IN</a></li>
-        <li class="list-inline-item mx-2"><a href="get_started.php">Get Started</a></li>
-    </ul>
-
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                
+                    <ul class="navbar-nav mx-auto pl-5 ml-5">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="home.php">HOME</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="support.php">SUPPORT</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="pricing.php">PRICING</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="sign_in.php">SIGN IN</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="get_started.php"><span class="border rounded px-2 py-2">Get Started</span></a>
+                        </li>
+                    </ul>
+                
+            </div>
+        </nav>
+    </div>
 </header>
--- src/index.php
+++ src/index.php
@@ -7,10 +7,10 @@
     <meta name="keywords" content="organization, scheduling, teamwork, checklists " />
     <meta name="author" content="Mohamed Abdelkoddous" />
     <title>Bamzooka | Home</title>
-    <link rel="stylesheet" type="text/css" href="src/csscode.css" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
-    <script src="https://kit.fontawesome.com/87d1c020bf.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
+    <link rel="stylesheet" type="text/css" href="./src/csscode.css" />
+
 </head>
 
 <body>
@@ -18,7 +18,7 @@
     <?php include "header.php" ?>
 
     <section role="try out" id="try_out" class="py-5">
-        <div class="mx-auto py-5 py-5" style="width: 800px;">
+        <div class="container mx-auto py-5 py-5">
             <h1 style="font-size: 45px; line-height: 60px;">Stop the Chaos. Organize Team Work with Process Checklists
             </h1>
             <p class="pb-5" style="font-size: 17px;">Create checklists, schedule them, assign them to your team, watch
@@ -26,7 +26,7 @@
             <div class="row">
                 <form class="form-inline m-auto" method="post" action="send_mail.php">
                     <div class="form-group mb-2">
-                        <input type="text" class="form-control" name="email" placeholder="Your work email">
+                        <input type="text" class="form-control ml-1" name="email" placeholder="Your work email">
                     </div>
                     <button type="submit" class="btn btn-light ml-2 mb-2" style="color:#cf4647; font-weight:700">
                         TRY FOR FREE</button>
@@ -39,41 +39,40 @@
     <section role="describe" id="describe" class="py-5 my-5">
         <div class="container">
             <div class="row" style="text-align:center">
-                <div class="col">
+                <div class="col-12 col-md-4 col-sm-12">
                     <i class="far fa-window-restore fa-6x"></i>
                     <h4 class="pt-4">Checklists & Processes</h4>
                     <p class="description">Describe processes as checklists. E.g.: Employee on-boarding checklist.</p>
                 </div>
-                <div class="col">
+                <div class="col-12 col-md-4 col-sm-12">
                     <i class="fas fa-cogs fa-6x"></i>
                     <h4 class="pt-4">Calendar</h4>
                     <p class="description">Start checklists anytime or use the built-in calendar to schedule repetitive
                         tasks.</p>
                 </div>
-                <div class="col">
+                <div class="col-12 col-md-4 col-sm-12">
                     <i class="fas fa-globe-americas fa-6x"></i>
                     <h4 class="pt-4">Monitor</h4>
                     <p class="description">Watch progress in realtime and see team assignments status.</p>
                 </div>
             </div>
             <div class="separator">
-                <div class="col-3"
-                    style="margin:60px; margin-left:auto; margin-right:auto; border-top:1px solid #cf4747">
+                <div class="col-3" style="margin:60px; margin-left:auto; margin-right:auto; border-top:1px solid #cf4747">
                 </div>
             </div>
             <div class="row" style="text-align:center">
-                <div class="col">
+                <div class="col-12 col-md-4 col-sm-12">
                     <i class="fas fa-thumbs-up fa-6x"></i>
                     <h4 class="pt-4">Pass / Fail Checks</h4>
                     <p class="description">Pass / fail checks can be configured to ensure work done meets certain
                         standards.</p>
                 </div>
-                <div class="col">
+                <div class="col-12 col-md-4 col-sm-12">
                     <i class="fas fa-lock fa-6x"></i>
                     <h4 class="pt-4">Security</h4>
                     <p class="description">Users are grouped into teams with specific access rights.</p>
                 </div>
-                <div class="col">
+                <div class="col-12 col-md-4 col-sm-12">
                     <i class="fas fa-code-branch fa-6x"></i>
                     <h4 class="pt-4">Smart</h4>
                     <p class="description">Configure checklists with conditional logic.</p>
@@ -83,7 +82,7 @@
     </section>
 
     <section role="quote" id="quote" class="py-5">
-        <div class="mx-auto" style="width: 700px;">
+        <div class="container mx-auto">
             <h3 class="pt-5 mt-5">"Bamzooka helps document processes, organize work and improve quality of everything
                 our teams do. It solves quality, consistency, reliability and turn-over challenges many organizations
                 face so that the work is done on time with predictable quality. It is a huge productivity gain for any
@@ -93,6 +92,11 @@
     </section>
 
     <?php include "footer.php" ?>
+
+    <script src="https://kit.fontawesome.com/87d1c020bf.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
 </body>
 
